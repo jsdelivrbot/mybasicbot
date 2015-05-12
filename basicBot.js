37cb2f4ec08449cb1e38f9096b1169124d4f98f9
cooljddj <cooljddj@gmail.com>
2015-05-12 16:41:04 +1000

    Uptodate to BasicBot(For trap bot's benifits)

--- basicBot.js
+++ basicBot.js
@@ -1640,7 +1640,7 @@
 
             cycleCommand: {
                 command: 'cycle',
-                rank: 'manager',
+                rank: 'mod',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -1957,7 +1957,7 @@
             },
 
             killCommand: {
-                command: 'kill',
+                command: 'mod',
                 rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
@@ -2143,7 +2143,7 @@
 
             lockskipposCommand: {
                 command: 'lockskippos',
-                rank: 'manager',
+                rank: 'mod',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
