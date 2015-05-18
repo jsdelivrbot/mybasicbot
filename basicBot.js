518df8aaa4a7f3c75d14d9908aeec1e26a3cca75
cooljddj <cooljddj@gmail.com>
2015-05-18 10:27:21 +1000

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -183,7 +183,7 @@
         name: "basicBot",
         loggedInID: null,
         scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
-        cmdLink: "http://git.io/245Ppg",
+        cmdLink: "http://bit.ly/1HpnshV",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
